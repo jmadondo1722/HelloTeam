a37020bcd878c40e96e675b40de1ca327bf24198
Craigen Chitate <craigenchitate7@gmail.com>
2024-01-31 19:58:48 +0200

    added my name

--- helloteam.js
+++ helloteam.js
@@ -1,3 +1,3 @@
 // helloteam.js
-const teamNames = ["Josiah", "Craigen Chitate", "Alex"];
+const teamNames = ["Josiah", "Craigen ", "Alex"];
 teamNames.forEach((teamNames) => console.log(`Hello ${teamNames}`));
