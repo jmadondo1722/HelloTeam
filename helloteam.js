b11418ffb15f0466fa82e7aa15953c49b4e350ac
Ayden Overson <ayden99@gmail.com>
2024-01-31 12:04:13 -0700

    added my name

--- helloteam.js
+++ helloteam.js
@@ -1,3 +1,3 @@
 // helloteam.js
-const teamNames = ["Josiah", "Craigen ", "Alex", "Karen"];
+const teamNames = ["Josiah", "Craigen ", "Alex", "Karen", "Ayden"];
 teamNames.forEach((teamNames) => console.log(`Hello ${teamNames}`));
